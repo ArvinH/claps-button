2c0821ada69419f9f8d674dc2a70f7174a7aab63
黃首翰（ArvinH） <arvin0731@gmail.com>
2020-03-29 18:34:15 +0900

    fix: remove unused method

--- src/components/claps-button/claps-button.tsx
+++ src/components/claps-button/claps-button.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Method, Prop, State, Listen, h, Host } from '@stencil/core';
+import { Component, Element, Prop, State, Listen, h, Host } from '@stencil/core';
 
 @Component({
   tag: 'claps-button',
